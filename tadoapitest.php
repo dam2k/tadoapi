d38e78577bc489df1ceeb00b81e9318922d488c8
Dino Ciuffetti <dam2000@gmail.com>
2025-03-08 00:08:42 +0100

    new version with device code grant flow

--- tadoapitest.php
+++ tadoapitest.php
@@ -1,13 +1,18 @@
 <?php
 /**
  * Author: Dino Ciuffetti - <dino@tuxweb.it>
- * Release date: 2023-08-04
+ * Release date: 2025-03-07
  * License: MIT
  * NOTE: TadoApi is an unofficial TADO (tm) SDK implementation for PHP (only read only methods). It's a cool way to export metrics from your thermostats.
  *
  * TADO (tm) does not support its public api in no way. I get the api methods from a tado knowledgebase public post.
  * Also, thank to this post: https://shkspr.mobi/blog/2019/02/tado-api-guide-updated-for-2019/
  * It's working for me, may be this is also ok for you. Like any other open source software, the author cannot assume any warranty.
+ *
+ * NOTE: TADO requested all its unofficial REST API users to change the authentication method for security reasons.
+ * This library implements the new device code grant flow with automatic refresh token and access token handling.
+ * By design of how the auth scheme works, the first time, you need to authenticate with your tado credentials using your browser.
+ * At this time this API will put the required url on STDERR, I'll work on a log implementation on my spare time.
  */
 declare(strict_types=1);
 
@@ -16,14 +21,10 @@ use dAm2K\TadoApi;
 require "vendor/autoload.php";
 
 $tadoconf = [
-	// Tado client ID and secret from https://my.tado.com/webapp/env.js
-	'tado.clientId' => 'tado-web-app',
-	'tado.clientSecret' => 'taG9tXxzGrIFWixUT1nZnzIjlovENGe0KNAB51ADKZQjSlNBvhs0xbT6tC4jIUaC',
-	'tado.username' => 'yourtadoemail@email.com',
-	'tado.password' => 'yourtadopassporcoziochenotiziamacomelhamessadentroguarda',
-	'tado.homeid' => '36389',
-	// your home's ID.
-	'statefile' => '/tmp/dam2ktado_aeSh8aem.txt' // we put access token here. When the AT expires a new one get collected and saved here
+	// this is the uuid they require: https://support.tado.com/en/articles/8565472-how-do-i-authenticate-to-access-the-rest-api
+	'tado.clientId' => '1bb50063-6b0c-4d11-bd99-387f4a91cc46',
+	'tado.homeid' => '36389', // your home's ID.
+	'statefile' => '/tmp/dam2ktado_aeSh8aem.txt' // we put device code, access and refresh tokens here. On expiration, new tokens are saved here
 ];
 
 $tado = new TadoApi($tadoconf);
