ab2b7a988a737633234bb20a5281bd7cee1d39d3
Dino Ciuffetti <dam2000@gmail.com>
2025-03-08 00:14:06 +0100

    new version with device code grant flow

--- src/TadoApi.php
+++ src/TadoApi.php
@@ -13,111 +13,201 @@ declare(strict_types=1);
 
 namespace dAm2K;
 
-use League\OAuth2\Client\Provider\GenericProvider;
 use GuzzleHttp\Client;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
+use GuzzleHttp\Exception\ClientException;
 
 class TadoApi
 {
 	private const TADO_MYAPI_BASEURI = 'https://my.tado.com/api/v2/';
 	private array $config; // configuration
-	private GenericProvider $provider; // tado oauth provider
-	private string $access_token; // jwt access token taken from oauth provider
+	//private GenericProvider $provider; // oauth provider for TADO API
+	private string $access_token;
+	private string $refresh_token;
+	private string $device_code; 
+	private int $device_code_retry_interval;
+	private \DateTimeImmutable $device_code_expires;
+	private \DateTimeImmutable $access_token_expires;
 	private string $homeId; // just bind to a single home id
 	private Client $client; // HTTP Client
 
-	// true if expiration time is valid, false if expired or invalid
-	private function checkJwtExpiration(string $access_token = ""): bool
-	{
-		$tks = explode('.', $access_token);
-		if (empty($tks))
-			return false;
-		if (count($tks) < 3)
-			return false;
-		list($headb64, $bodyb64, $cryptob64) = $tks;
-		$payload = JWT::jsonDecode(JWT::urlsafeB64Decode($bodyb64));
-		if (!isset($payload->exp))
-			return false;
-		$current_date = new \DateTime();
-		$jwt_date = \DateTime::createFromFormat('U', (string) $payload->exp);
-		if ($current_date > $jwt_date)
-			return false; // expired
-		return true; // not expired
-	}
-
-	private function getStateFile()
-	{ // get state data from state file
+	private function getStateFile(): void { // get state data from state file
 		if (is_readable($this->config['statefile'])) {
 			$state = json_decode(file_get_contents($this->config['statefile']));
 			if (!empty($state->access_token))
 				$this->access_token = $state->access_token;
+			if (!empty($state->refresh_token))
+				$this->refresh_token = $state->refresh_token;
+			if (!empty($state->device_code))
+				$this->device_code = $state->device_code;
+			if (!empty($state->device_code_expires))
+				$this->device_code_expires = new \DateTimeImmutable($state->device_code_expires);
+			if (!empty($state->access_token_expires))
+				$this->access_token_expires = new \DateTimeImmutable($state->access_token_expires);
+			if (!empty($state->device_code_retry_interval))
+				$this->device_code_retry_interval = $state->device_code_retry_interval;
 		}
 	}
 
-	private function setStateFile()
-	{ // set state data to state file
+	private function setStateFile():void { // set state data to state file
 		$state = [
-			'access_token' => $this->access_token
+			'access_token' => $this->access_token,
+			'refresh_token' => $this->refresh_token,
+			'device_code' => $this->device_code,
+			'device_code_expires' => $this->device_code_expires->format(\DateTimeImmutable::ATOM),
+			'access_token_expires' => $this->access_token_expires->format(\DateTimeImmutable::ATOM),
+			'device_code_retry_interval' => $this->device_code_retry_interval
 		];
 		file_put_contents($this->config['statefile'], json_encode((object) $state));
 	}
-
-	private function renewAccessTokenIfNeeded()
-	{
-		if (!$this->checkJwtExpiration($this->access_token)) { // access token invalid or expired
-			// get a new access token
-			$this->access_token = $this->provider->getAccessToken('password', [
-				'username' => $this->config['tado.username'],
-				'password' => $this->config['tado.password'],
-				'scope' => 'home.user',
-			])->getToken();
-			$this->setStateFile();
-			return;
-		}
-		// using old access token
-	}
-
-	private function getData(string $method, string $url): \stdClass|array
-	{
-		$this->renewAccessTokenIfNeeded();
+	
+	private function getData(string $method, string $url): \stdClass|array {
+		$this->getAccessToken();
 		$options['headers']['content-type'] = 'application/json';
-		$request = $this->provider->getAuthenticatedRequest($method, $url, $this->access_token, $options);
-		$response = $this->client->send($request);
+		$options['headers']['Authorization'] = 'Bearer '.$this->access_token;
+		$response = $this->client->request($method, $url, $options);
 		$responseContents=$response->getBody()->getContents();
 		if(empty($responseContents)) return [];
 		return json_decode($responseContents);
 	}
-
-	private function setData(string $method, string $url, string $data): \stdClass|array
-	{
-		$this->renewAccessTokenIfNeeded();
+	
+	private function setData(string $method, string $url, string $data): \stdClass|array {
+		$this->getAccessToken();
 		$options['headers']['content-type'] = 'application/json';
+		$options['headers']['Authorization'] = 'Bearer '.$this->access_token;
 		$options['body']=$data;
-		$request = $this->provider->getAuthenticatedRequest($method, $url, $this->access_token, $options);
-		$response = $this->client->send($request);
+		$response = $this->client->request($method, $url, $options);
 		$responseContents=$response->getBody()->getContents();
 		if(empty($responseContents)) return [];
 		return json_decode($responseContents);
 	}
-
-	public function __construct(array $config, string $access_token = "")
+	
+	/**
+	 * Get deviceCode from the stateFile (if not expired), or create a new one to be authenticated by the user's browser.
+	 */
+	private function getDeviceCode(): void {
+		$cd = new \DateTimeImmutable('now', new \DateTimeZone(date_default_timezone_get()));
+		if(!empty($this->device_code) && !empty($this->device_code_expires && !empty($this->device_code_retry_interval))) { // device code got from the state file
+			if($cd < $this->device_code_expires) { // device code has NOT expired
+				return;
+			} // device code expired before user auth. Request another one
+		}
+		
+		$response = $this->client->post('https://login.tado.com/oauth2/device_authorize', [
+			'form_params' => [
+				'client_id' => $this->config['tado.clientId'],
+				'scope'     => 'offline_access',
+			],
+		]);
+		
+		/* {"device_code":"ftcrinX_KQNXUNI1wkh-5zxMmmYOUug43SAYWORssAU","expires_in":300,"interval":5,"user_code":"9HBZPZ","verification_uri":"https://login.tado.com/oauth2/device","verification_uri_complete":"https://login.tado.com/oauth2/device?user_code=9HBZPZ"} */
+		
+		$data = json_decode($response->getBody()->getContents(), true);
+		$this->device_code = $data['device_code'];
+		$this->device_code_retry_interval = $data['interval'];
+		$ed = new \DateTimeImmutable('now', new \DateTimeZone(date_default_timezone_get()));
+		$ed = $ed->add(new \DateInterval('PT'.$data['expires_in'].'S'));
+		$this->device_code_expires = $ed;
+		$this->setStateFile();
+		// Show the user the authorization link to be executed on its browser with its tado credentials.
+		throw new TadoApiDeviceCodeAuthException("deviceCode ". $this->device_code ." must be manually verified by a browser. Please, go to: " . $data['verification_uri_complete'] . " before " . $ed->format('Y-m-d H:i:s') . " and put in this user code to the web form: " . $data['user_code']);
+		return;
+	}
+	
+	/**
+	 * Get access token and refresh token from the stateFile, create access token from refresh token or request new tokens if refresh is not valid
+	 */
+	private function getAccessToken(): void {
+		$cd = new \DateTimeImmutable('now', new \DateTimeZone(date_default_timezone_get()));
+		if(!empty($this->access_token) && !empty($this->refresh_token) && !empty($this->access_token_expires)) { // got access token from the state file
+                        if($cd < $this->access_token_expires) { // access token has NOT expired
+				// we hope that the access token is valid, because it's not expired
+                                return;
+                        } // access token expired. Request a new access token with the refresh token, hoping the refresh token it's still valid
+			$response = $this->client->post('https://login.tado.com/oauth2/token', [
+				'form_params' => [
+					'client_id' => $this->config['tado.clientId'],
+					'grant_type' => 'refresh_token',
+					'refresh_token' => $this->refresh_token
+				],
+			]);
+			$data = json_decode($response->getBody()->getContents(), true);
+			if (isset($data['access_token'])) {
+				$this->access_token=$data['access_token'];
+				$this->refresh_token=$data['refresh_token'];
+				$ed = new \DateTimeImmutable('now', new \DateTimeZone(date_default_timezone_get()));
+				$ed = $ed->add(new \DateInterval('PT'.$data['expires_in'].'S'));
+				$this->access_token_expires = $ed;
+				$this->setStateFile();
+				return;
+			}
+                }
+		
+		$interval = $this->device_code_retry_interval ?? 5; // sleep 5 seconds between each retry
+		$maxAttempts = 120 / $interval; // retry for about two minutes in case we need the browser to authenticate
+		for($i=0; $i<$maxAttempts; $i++) {
+			try {
+				$this->getDeviceCode(); // we will get this one from the state file, or a new one if expired
+				$response = $this->client->post('https://login.tado.com/oauth2/token', [
+					'form_params' => [
+						'client_id' => $this->config['tado.clientId'],
+						'grant_type' => 'urn:ietf:params:oauth:grant-type:device_code',
+						'device_code' => $this->device_code
+					],
+				]);
+				$data = json_decode($response->getBody()->getContents(), true);
+				if (isset($data['access_token'])) {
+					/*
+					echo "Access Token: " . $data['access_token'] . "\n";
+					echo "Refresh Token: " . $data['refresh_token'] . "\n";
+					echo "Expires in: " . $data['expires_in'] . " secondi\n";
+					*/
+					/*
+					[access_token] => eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImd0eSI..............................KcYbYQ
+					[expires_in] => 599
+					[refresh_token] => 6Vu0vQadysY-1G6n6R8gdp_y-TgFtakh72C7KVKN-uUxgbM3EWHTzxTe2D6ZD88W
+					[refresh_token_id] => 9f75bb86-8d55-4778-9268-313bbd1bc1a5
+					[scope] => offline_access
+					[token_type] => Bearer
+					[userId] => 595e4511-098f-8000-3325-0adc23930000
+					*/
+				}
+				$this->access_token=$data['access_token'];
+				$this->refresh_token=$data['refresh_token'];
+				$ed = new \DateTimeImmutable('now', new \DateTimeZone(date_default_timezone_get()));
+				$ed = $ed->add(new \DateInterval('PT'.$data['expires_in'].'S'));
+				$this->access_token_expires = $ed;
+				$this->setStateFile();
+				return;
+			} catch (ClientException $e) {
+				$errorResponse = json_decode($e->getResponse()->getBody()->getContents(), true);
+				if ($errorResponse['error'] === 'authorization_pending') {
+					fprintf(STDERR, "%s", "Still pending user OAUTH2 authorization. Device code expires at: ".$this->device_code_expires->format('Y-m-d H:i:s')."\n");
+				} else {
+					throw new \Exception($errorResponse['error_description']);
+				}
+			} catch (TadoApiDeviceCodeAuthException $e) {
+				fprintf(STDERR, "%s\n", $e->getMessage());
+			}
+			sleep($interval);
+		}
+		throw new TadoApiDeviceCodeAuthException("Cannot authenticate with device code to obtain refresh and access tokens.");
+	}
+	
+	public function __construct(array $config)
 	{
 		$this->config = $config;
 		$this->access_token = "";
+		$this->refresh_token = "";
+		$this->access_token_expires = new \DateTimeImmutable();
+		$this->access_token_expires = $this->access_token_expires->sub(new \DateInterval('P1Y')); // 1 year ago
+		//$this->device_code_expires=new \DateTimeImmutable;
+		$this->device_code = "";
+		//$this->device_code_retry_interval=5;
 		$this->getStateFile();
-		if (!empty($access_token))
-			$this->access_token = $access_token;
+		// TODO: write how to find Tado homeId
 		$this->homeId = $config['tado.homeid'];
 		$this->client = new Client();
-		$this->provider = new GenericProvider([
-			'clientId' => $this->config['tado.clientId'],
-			'clientSecret' => $this->config['tado.clientSecret'],
-			'urlAuthorize' => 'https://auth.tado.com/oauth/authorize',
-			'urlAccessToken' => 'https://auth.tado.com/oauth/token',
-			'urlResourceOwnerDetails' => null,
-		]);
-		// not checking access token now
+		$this->getAccessToken();
 	}
 
 	public function getMe(): \stdClass
@@ -299,4 +389,10 @@ class TadoApi
 		$data->zones = array_values($data->zones);
 		return $data;
 	}
+
+        public function disableVSwing(string $homeId = "", string $zoneId): \stdClass
+        {
+                return $this->getData('PUT', self::TADO_MYAPI_BASEURI . 'homes/' . (!empty($homeId) ? $homeId : $this->homeId) . '/zones/' . $zoneId . '/overlay');
+        }
+
 }
